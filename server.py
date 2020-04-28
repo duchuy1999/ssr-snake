b5101e96d7ba3d35ce6d1154c13c2a183a8359b4
Huy Nguyen <duchuy1999@yahoo.com>
2020-04-28 01:18:32 -0700

    Change snake design

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ class Battlesnake(object):
         # TODO: Use this function to decide how your snake is going to look on the board.
         data = cherrypy.request.json
         print("START")
-        return {"color": "#888888", "headType": "regular", "tailType": "regular"}
+        return {"color": "#FFF520", "headType": "shac-caffeine", "tailType": "shac-mouse"}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
